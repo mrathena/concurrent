ff59479eb23280f49e8c06ff59c16792922aa99a
mrathena <747779761@qq.com>
2017-08-29 16:56:13 +0800

    并发基础

---
+++ src/main/java/com/mrathena/concurrent/executor/ExecutorsDemo.java
@@ -0,0 +1,11 @@
+package com.mrathena.concurrent.executor;
+
+public class ExecutorsDemo {
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+}
--- src/main/java/com/mrathena/concurrent/executor/ScheduledExecutorServiceDemo.java
+++ src/main/java/com/mrathena/concurrent/executor/ScheduledExecutorServiceDemo.java
@@ -1,8 +1,9 @@
 package com.mrathena.concurrent.executor;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 import com.mrathena.concurrent.tool.ThreadKit;
@@ -21,18 +22,49 @@ public class ScheduledExecutorServiceDemo {
 	public static void main(String[] args) {
 		try {
 			ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+
+			// schedule(Runnable, , ), 返回值只能是null, 可以不用管
+//			executor.schedule(new Runnable() {
+//				@Override
+//				public void run() {
+//					ThreadKit.sleep(1000);
+//				}
+//			}, 5L, TimeUnit.SECONDS);// 5秒后执行该线程
+
+			// schedule(Callable<V>, , ) 
+//			ScheduledFuture<String> future2 = executor.schedule(new Callable<String>() {
+//				@Override
+//				public String call() throws Exception {
+//					ThreadKit.sleep(1000);
+//					return "success";
+//				}
+//			}, 5L, TimeUnit.SECONDS);
+//			System.out.println(future2.get());
 			
-			// schedule(Runnable, , ) with return null
-			@SuppressWarnings("rawtypes")
-			ScheduledFuture future =  executor.schedule(new Runnable() {
+			// scheduleAtFixedRate(Runnable, , , )
+			// 创建并执行一个在给定初始延迟后首次启用的定期操作，后续操作具有给定的周期；
+			// 也就是将在 initialDelay 后开始执行，然后在 initialDelay+period 后执行(不管上一次有没有执行完)，
+			// 接着在 initialDelay + 2 * period 后执行(不管上一次有没有执行完)，依此类推。
+			executor.scheduleAtFixedRate(new Runnable() {
 				@Override
 				public void run() {
-					ThreadKit.sleep(1000);
+					SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+					System.out.println("A: " + sdf.format(new Date()));
 				}
-			}, 5L, TimeUnit.SECONDS);// 5秒后执行该线程
-			System.out.println(future.get());
+			}, 2L, 1L, TimeUnit.SECONDS);
 			
-			
+			// scheduleWithFixedDelay(Runnable, , , )
+			// 创建并执行一个在给定初始延迟后首次启用的定期操作，后续操作具有给定的周期；
+			// 也就是将在 initialDelay 后开始执行，然后在上一次执行完后 再过delay 后执行，
+			// 接着在上一次执行完后 再过delay 后执行，依此类推。
+			executor.scheduleWithFixedDelay(new Runnable() {
+				@Override
+				public void run() {
+					ThreadKit.sleep(1100);
+					SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+					System.out.println("B: " + sdf.format(new Date()));
+				}
+			}, 2L, 1L, TimeUnit.SECONDS);
 			
 		} catch (Exception e) {
 			e.printStackTrace();
